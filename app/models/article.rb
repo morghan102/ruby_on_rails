8d3d9aaf1d0e10125618f068926c792349eeb947
Morghan <morghan102@gmail.com>
2019-11-24 15:55:01 -0500

    finished blog comments feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
 #knows that it shd have a title&body bc of "reflection"
 #rails queries database, looks @articles tabel, & assumes whtvr
 #tbles it has shd be atributes for the model
+    has_many :comments
 end
